f01c844d66064c290068a3d908351ca3f784c0fe
Yann MARCOU <creativeyann17@gmail.com>
2023-02-14 14:05:22 -0500

    feat: CLIN-1723 - add POLYM

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ echo "=== COPY TO S3 ($ENV) ==="
 aws --profile cqgc-$ENV --endpoint https://s3.cqgc.hsj.rtss.qc.ca s3 cp ./data/output/$FILE s3://cqgc-$ENV-app-datalake/raw/landing/panels/$FILE
 aws --profile cqgc-$ENV --endpoint https://s3.cqgc.hsj.rtss.qc.ca s3 cp ./data/output/$FILE s3://cqgc-$ENV-app-datalake/raw/landing/panels/panels.tsv
 
-echo "=== JARS IN S3 ($ENV) ==="
+echo "=== PANELS IN S3 ($ENV) ==="
 aws --profile cqgc-$ENV --endpoint https://s3.cqgc.hsj.rtss.qc.ca s3 ls s3://cqgc-$ENV-app-datalake/raw/landing/panels/ --recursive
--- src/main/java/org/clin/panels/Main.java
+++ src/main/java/org/clin/panels/Main.java
@@ -1,12 +1,19 @@
 package org.clin.panels;
 
+import java.io.IOException;
+
 public class Main {
 
   public static void main(String[]args) throws Exception {
+    generate20230203();
+    generate20230214();
+  }
+
+  private static void generate20230203() throws IOException {
     var model = Parser.parse("./data/output/panels_20230202.tsv");
 
-    var rgdiPlus = Parser.parse(Parser.TSV, "./data/input/RGDI+ - Sheet1.tsv");
-    var rgdiV2 = Parser.parse(Parser.TSV, "./data/input/RGDI_v2 - Sheet1.tsv");
+    var rgdiPlus = Parser.parse(Parser.TSV, "./data/input/RGDI+.tsv");
+    var rgdiV2 = Parser.parse(Parser.TSV, "./data/input/RGDI_v2.tsv");
 
     rgdiPlus.stream().iterator().forEachRemaining((r) -> {
       model.add(r.get(0), "RGDI+", "RGDI+_v1");
@@ -19,6 +26,19 @@ public class Main {
     System.out.println(model.getDistinctPanels());
 
     Parser.write(model, "./data/output/panels_20230203.tsv");
+  }
 
+  private static void generate20230214() throws IOException {
+    var model = Parser.parse("./data/output/panels_20230203.tsv");
+
+    var polymV1 = Parser.parse(Parser.TSV, "./data/input/POLYM_v1.tsv");
+
+    polymV1.stream().iterator().forEachRemaining((r) -> {
+      model.add(r.get(0), "POLYM", "POLYM_v1");
+    });
+
+    System.out.println(model.getDistinctPanels());
+
+    Parser.write(model, "./data/output/panels_20230214.tsv");
   }
 }
